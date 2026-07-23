27906d4bd4f17c319e6700ebfa1115efd83c0ee5
Hoai Phong - Alex <alexhoaiphong@gmail.com>
2026-07-22 21:45:54 -0700

    update config in backend

--- src/api/schemas.py
+++ src/api/schemas.py
@@ -1,11 +1,20 @@
 from pydantic import BaseModel, Field
 from src.schema.job_description import JobDescriptionDraft
 from src.schema.job_info import JobInfo
+import json 
+import os
 
-# PROVIDER = 'huggingface'
-# MODEL = 'HuggingFaceH4/zephyr-7b-beta'
-PROVIDER = 'openai'
-MODEL = 'gpt-5.5'
+config_path = os.path.join (os.path.dirname(__file__), '../config.json')
+try: 
+    with open (config_path, 'r') as file: 
+        config = json.load (file)
+    PROVIDER = config.get ("LLM_PROVIDER")
+    MODEL = config.get ("LLM_MODEL")
+except Exception as e:
+    print (f"Warning: Could not load config.json. Using defaults. Erro: {e}")
+    PROVIDER = 'huggingface'
+    MODEL = 'Qwen/Qwen2.5-7B-Instruct'
+
 class QuestionResponse (BaseModel):
     question_name: str
     question_text: str
